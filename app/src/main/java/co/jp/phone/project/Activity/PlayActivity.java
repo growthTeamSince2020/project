1150bf20493b19c4b614ceaf82996e82d033165f
iwasuji <t.iwasuji0910@gmail.com>
2020-12-26 17:36:38 +0900

    2020/12/12 dao（entity）を作成

--- app/src/main/java/co/jp/phone/project/Activity/PlayActivity.java
+++ app/src/main/java/co/jp/phone/project/Activity/PlayActivity.java
@@ -1,6 +1,8 @@
 package co.jp.phone.project.Activity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.media.AudioAttributes;
@@ -17,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import co.jp.phone.project.Constant.DatabeseHelper;
 import co.jp.phone.project.Constant.telNumberConst;
@@ -42,87 +45,93 @@ public class PlayActivity extends AppCompatActivity {
     private final String se3 = "3";
     private final String se4 = "4";
 
-    /** シングルクォート*/
-    private final String sQuote= "'";
+    /**
+     * シングルクォート
+     */
+    private final String sQuote = "'";
 
-    /** ボタン入力情報を設定*/
-    public void on_ac_telButton_Click(View view){
-        if(view.getId() == R.id.ac_bottom){
+    /**
+     * ボタン入力情報を設定
+     */
+    public void on_ac_telButton_Click(View view) {
+        if (view.getId() == R.id.ac_bottom) {
             this.clearText();
-        }else if (view.getId() == R.id.tel_Bottom){
+        } else if (view.getId() == R.id.tel_Bottom) {
             this.numberLoad();
-        }else if(view.getId() == R.id.tel_bottom1){
+        } else if (view.getId() == R.id.tel_bottom1) {
             this.init(telNumberConst.button1);
-        }else if(view.getId() == R.id.tel_bottom2){
+        } else if (view.getId() == R.id.tel_bottom2) {
             this.init(telNumberConst.button2);
-        }else if(view.getId() == R.id.tel_bottom3){
+        } else if (view.getId() == R.id.tel_bottom3) {
             this.init(telNumberConst.button3);
-        }else if(view.getId() == R.id.tel_bottom4){
+        } else if (view.getId() == R.id.tel_bottom4) {
             this.init(telNumberConst.button4);
-        }else if(view.getId() == R.id.tel_bottom5){
+        } else if (view.getId() == R.id.tel_bottom5) {
             this.init(telNumberConst.button5);
-        }else if(view.getId() == R.id.tel_bottom6){
+        } else if (view.getId() == R.id.tel_bottom6) {
             this.init(telNumberConst.button6);
-        }else if(view.getId() == R.id.tel_bottom7){
+        } else if (view.getId() == R.id.tel_bottom7) {
             this.init(telNumberConst.button7);
-        }else if(view.getId() == R.id.tel_bottom8){
+        } else if (view.getId() == R.id.tel_bottom8) {
             this.init(telNumberConst.button8);
-        }else if(view.getId() == R.id.tel_bottom9){
+        } else if (view.getId() == R.id.tel_bottom9) {
             this.init(telNumberConst.button9);
-        }else if(view.getId() == R.id.tel_bottom0){
+        } else if (view.getId() == R.id.tel_bottom0) {
             this.init(telNumberConst.button0);
-        }else if(view.getId() == R.id.tel_bottom_asta){
+        } else if (view.getId() == R.id.tel_bottom_asta) {
             this.init(telNumberConst.button_asta);
-        }else if(view.getId() == R.id.tel_bottom_sharp){
+        } else if (view.getId() == R.id.tel_bottom_sharp) {
             this.init(telNumberConst.button_sharp);
         }
     }
 
     /*EditTextクリア処理*/
     private void clearText() {
-        EditText editText =(EditText)findViewById(R.id.text);
+        EditText editText = (EditText) findViewById(R.id.text);
         String clearTxt = editText.getText().toString();
         clearTxt = "";
         editText.setHeight(12);
         editText.setText(clearTxt, EditText.BufferType.NORMAL);
-   
+
     }
 
 
     // BGM準備
     //BGMPlayer bgmPlayer = new BGMPlayer();
     MediaPlayer bgmPlayer = new MediaPlayer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        editText =(EditText)findViewById(R.id.text);
+        editText = (EditText) findViewById(R.id.text);
         editText.setTextColor(Color.RED);
         editText.setFocusable(false);
 
         /** １０円玉の画像を取得*/
-        ImageView iv_coin1 = (ImageView)findViewById(R.id.coin1);
-        ImageView iv_coin2 = (ImageView)findViewById(R.id.coin2);
-        ImageView iv_coin3 = (ImageView)findViewById(R.id.coin3);
+        ImageView iv_coin1 = (ImageView) findViewById(R.id.coin1);
+        ImageView iv_coin2 = (ImageView) findViewById(R.id.coin2);
+        ImageView iv_coin3 = (ImageView) findViewById(R.id.coin3);
 
         /** ワークテーブルから情報を取得*/
         int record_count = wkTbList();
 
-        if (record_count == 1){
+        if (record_count == 1) {
             /** １０円玉１枚分を非表示*/
             iv_coin3.setVisibility(View.GONE);
-        } else if(record_count == 2){
+        } else if (record_count == 2) {
             /** １０円玉２枚分を非表示*/
             iv_coin2.setVisibility(View.GONE);
             iv_coin3.setVisibility(View.GONE);
         }
 
-        bgmPlayer.create(getApplicationContext(),R.raw.bgm_higurashi);
+        bgmPlayer.create(getApplicationContext(), R.raw.bgm_higurashi);
 
     }
 
     /**
-     *  ワークテーブルにレコードが存在するか確認
+     * ワークテーブルにレコードが存在するか確認
+     *
      * @return
      */
     private int wkTbList() {
@@ -135,18 +144,18 @@ public class PlayActivity extends AppCompatActivity {
         DatabeseHelper dbHelper = new DatabeseHelper(this);
         SQLiteDatabase wk = dbHelper.getReadableDatabase();
 
-        try{
+        try {
             /** SQL文を実行*/
-            Cursor cursor = wk.rawQuery(wkSql.toString(),null);
+            Cursor cursor = wk.rawQuery(wkSql.toString(), null);
 
             /** 件数をカウント*/
-             int ct = cursor.getCount();
+            int ct = cursor.getCount();
 
-             /** 取得件数を返す*/
-             return ct;
+            /** 取得件数を返す*/
+            return ct;
 
-        }catch (Exception ex){
-            Log.e("telWklistテーブル情報取得エラー",ex.toString());
+        } catch (Exception ex) {
+            Log.e("telWklistテーブル情報取得エラー", ex.toString());
         }
 
         return 99;
@@ -174,17 +183,17 @@ public class PlayActivity extends AppCompatActivity {
 
     /*入力番号をEditText蘭に表示
                 すでに入力されている情報がある場合は値を付加*/
-    private String init(String val){
+    private String init(String val) {
 
-        editText =(EditText)findViewById(R.id.text);
+        editText = (EditText) findViewById(R.id.text);
         editText.setHeight(12);
 
         String value;
         value = editText.getText().toString();
-        if(value.equals("")){
+        if (value.equals("")) {
 
             editText.setText(val, EditText.BufferType.NORMAL);
-        }else if(!value.equals("")){
+        } else if (!value.equals("")) {
             val = value + val;
             editText.setText(val, EditText.BufferType.NORMAL);
         }
@@ -194,21 +203,20 @@ public class PlayActivity extends AppCompatActivity {
 
 
     /*電話番号入力情報を取得*/
-    private String numberLoad(){
+    private String numberLoad() {
 
-        editText =(EditText)findViewById(R.id.text);
+        editText = (EditText) findViewById(R.id.text);
         editText.setHeight(12);
 
         String value;
         value = editText.getText().toString();
 
         //番号入力チェック
-        if(value.equals("")){
+        if (value.equals("")) {
             return null;
         }
 
         this.telPhoneTime(value);
-
 
 
         this.clearText();
@@ -217,12 +225,13 @@ public class PlayActivity extends AppCompatActivity {
 
     /**
      * 指定した電話番号先を呼び出す
-     * @param val1  入力された電話番号1
+     *
+     * @param val1 入力された電話番号1
      * @return
      */
-    private String telPhoneTime(String val1){
+    private String telPhoneTime(String val1) {
         /** nullチェック*/
-        if(val1.equals("")){
+        if (val1.equals("")) {
             return null;
         }
 
@@ -235,9 +244,9 @@ public class PlayActivity extends AppCompatActivity {
         Cursor dataList = wkDataList();
 
         /** countを設定する*/
-        if (record_count == 1){
+        if (record_count == 1) {
             count = count + 1;
-        } else if(record_count == 2){
+        } else if (record_count == 2) {
             count = count + 2;
         }
 
@@ -246,43 +255,51 @@ public class PlayActivity extends AppCompatActivity {
         /** ヘルパーからDB接続オブジェクトをもらう */
         SQLiteDatabase db = helper.getWritableDatabase();
 
-        StringBuilder sql = new StringBuilder();
-        Integer ct = new Integer(count);
-        try {
-        sql.append("select tpl.end_id ,tpl.message from TEL_PHONE_LIST tpl");
-        if(count == 1){
-            sql.append(" where tpl.count_number = " + "'" + count + "'");
-            sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
 
-            String[] str  ={ct.toString(),val1};
-            db.rawQuery(sql.toString(),str);
-        }else if(count == 2){
-            sql.append(" where tpl.count_number = " + "'" + count + "'");
-            sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
+        StringBuilder sql = new StringBuilder();
+        int rowcount = 0;
+        try {
+            sql.append("select tpl.end_id ,tpl.message from TEL_PHONE_LIST tpl");
+            if (count == 1) {
+                sql.append(" where tpl.count_number = " + "'" + count + "'");
+                sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
+            } else if (count == 2) {
+                sql.append(" where tpl.count_number = " + "'" + count + "'");
+                sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
 //            sql.append(" and tpl.tel_number2 = " + "'" + val2 + "'");
-            String[] str  ={ct.toString(),val1};
-            db.rawQuery(sql.toString(),str);
-        }else if(count == 3) {
-            sql.append(" where tpl.count_number = " + "'" + count + "'");
-            sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
+            } else if (count == 3) {
+                sql.append(" where tpl.count_number = " + "'" + count + "'");
+                sql.append(" and tpl.tel_number1 = " + "'" + val1 + "'");
 //            sql.append(" and tpl.tel_number2 = " + "'" + val2 + "'");
 //            sql.append(" and tpl.tel_number3 = " + "'" + val3 + "'");
-            String[] str  ={ct.toString(),val1};
-            db.rawQuery(sql.toString(),str);
-        }
+            }
+            SQLiteCursor c = (SQLiteCursor) db.rawQuery(sql.toString(), null);
+            rowcount = c.getCount();
+            StringBuffer sb = new StringBuffer();
 
+            for (int i = 0; i < rowcount; i++) {
+                String end_id = c.getString(0);
+                String message = c.getString(1);
 
+                sb.append("[" + end_id + ":" + message + "]");
+                c.moveToNext();
+            }
 
-        }catch (Exception ex){
-            Log.e("telPhoneTimeエラー",ex.toString());
-        }finally {
-            db.close();;
+            System.out.println(sb);
+
+        } catch (Exception ex) {
+            Log.e("telPhoneTimeエラー", ex.toString());
+        } finally {
+            db.close();
+            ;
         }
 
         return null;
     }
+
     /**
-     *  ワークテーブルから電話履歴情報を取得する。
+     * ワークテーブルから電話履歴情報を取得する。
+     *
      * @return
      */
     private Cursor wkDataList() {
@@ -295,17 +312,17 @@ public class PlayActivity extends AppCompatActivity {
         DatabeseHelper dbHelper = new DatabeseHelper(this);
         SQLiteDatabase wk = dbHelper.getReadableDatabase();
 
-        try{
+        try {
             /** SQL文を実行*/
-            Cursor cursor = wk.rawQuery(wkSql.toString(),null);
+            Cursor cursor = wk.rawQuery(wkSql.toString(), null);
 
 //            ArrayList<Cursor> arrayList = new ArrayList<>();
 //            arrayList.add(cursor);
 
             return cursor;
 
-        }catch (Exception ex){
-            Log.e("telWklistテーブル情報取得エラー",ex.toString());
+        } catch (Exception ex) {
+            Log.e("telWklistテーブル情報取得エラー", ex.toString());
         }
 
         return null;
